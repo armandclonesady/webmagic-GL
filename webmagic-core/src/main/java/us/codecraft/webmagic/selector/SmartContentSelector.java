dad0b3273995f7e65f8b285d1e0e78941ef27cc7
Raphaël Kiecken <raphael.kiecken.etu@univ-lille.fr>
2026-03-31 11:48:42 +0200

    Begin refactor SmartContentSelector to improve HTML cleaning and index distribution logic

--- webmagic-core/src/main/java/us/codecraft/webmagic/selector/SmartContentSelector.java
+++ webmagic-core/src/main/java/us/codecraft/webmagic/selector/SmartContentSelector.java
@@ -24,32 +24,66 @@ public class SmartContentSelector implements Selector {
 
     private int threshold;
 
+    private ArrayList<Integer> indexDistribution;
+
     public SmartContentSelector() {
         this.threshold = DEFAULT_THRESHOLD;
+        this.indexDistribution = new ArrayList<>();
     }
 
     public SmartContentSelector(int threshold) {
         this.threshold = threshold;
     }
 
-
-
     @Override
     public String select(String html) {
-        html = html.replaceAll("(?is)<!DOCTYPE.*?>", "");
+        String cleanHtml = cleanHtml(html);
+        List<String> lines = Arrays.asList(cleanHtml.split("\n"));
+        populateIndexDistribution(lines);
+
+        int start = -1;
+        int end = -1;
+        boolean boolstart = false;
+        boolean boolend = false;
+        StringBuilder text = new StringBuilder();
+        text.setLength(0);
+
+        for (int i = 0; i < indexDistribution.size() - 1; i++) {
+            if (indexIsAboveThreshold(i) && !boolstart && isStart(i)) {
+                boolstart = true;
+                start = i;
+                continue;
+            }
+            if (boolstart && isEnd(i)) {
+                end = i;
+                boolend = true;
+            }
+            StringBuilder tmp = new StringBuilder();
+            if (boolend) {
+                for (int ii = start; ii <= end; ii++) {
+                    if (lines.get(ii).length() < DEFAULT_MIN_LENGTH) continue;
+                    tmp.append(lines.get(ii) + "\n");
+                }
+                String str = tmp.toString();
+                if (str.contains("Copyright")   ) continue;
+                text.append(str);
+                boolstart = boolend = false;
+            }
+        }
+        return text.toString();
+    }
+
+    private String cleanHtml(String html) {
+        html = html.replaceAll("(?is)<!DOCTYPE[^>]*+>", "");
         html = html.replaceAll("(?is)<!--.*?-->", "");				// remove html comment
         html = html.replaceAll("(?is)<script.*?>.*?</script>", ""); // remove javascript
         html = html.replaceAll("(?is)<style.*?>.*?</style>", "");   // remove css
         html = html.replaceAll("&.{2,5};|&#.{2,5};", " ");			// remove special char
-        html = html.replaceAll("(?is)<.*?>", "");
-        List<String> lines;
-        int start;
-        int end;
-        StringBuilder text = new StringBuilder();
-        ArrayList<Integer> indexDistribution = new ArrayList<Integer>();
+        return html;
+    }
 
-        lines = Arrays.asList(html.split("\n"));
-
+    private void populateIndexDistribution(List<String> lines) {
+        this.indexDistribution.clear();
         for (int i = 0; i < lines.size() - DEFAULT_BLOCKS_WIDTH; i++) {
             int wordsNum = 0;
             for (int j = i; j < i + DEFAULT_BLOCKS_WIDTH; j++) {
@@ -58,44 +92,22 @@ public class SmartContentSelector implements Selector {
             }
             indexDistribution.add(wordsNum);
         }
+    }
 
-        start = -1; end = -1;
-        boolean boolstart = false;
-        boolean boolend = false;
-        text.setLength(0);
+    private boolean isStart(int index) {
+        return !indexIsZero(index + 1) && !indexIsZero(index + 2) && !indexIsZero(index + 3);
+    }
 
-        for (int i = 0; i < indexDistribution.size() - 1; i++) {
-            if (indexDistribution.get(i) > threshold && ! boolstart) {
-                if (indexDistribution.get(i+1).intValue() != 0
-                        || indexDistribution.get(i+2).intValue() != 0
-                        || indexDistribution.get(i+3).intValue() != 0) {
-                    boolstart = true;
-                    start = i;
-                    continue;
-                }
-            }
-            if (boolstart) {
-                if (indexDistribution.get(i).intValue() == 0
-                        || indexDistribution.get(i+1).intValue() == 0) {
-                    end = i;
-                    boolend = true;
-                }
-            }
-            StringBuilder tmp = new StringBuilder();
-            if (boolend) {
-                //System.out.println(start+1 + "\t\t" + end+1);
-                for (int ii = start; ii <= end; ii++) {
-                    if (lines.get(ii).length() < DEFAULT_MIN_LENGTH) continue;
-                    tmp.append(lines.get(ii) + "\n");
-                }
-                String str = tmp.toString();
-                //System.out.println(str);
-                if (str.contains("Copyright")   ) continue;
-                text.append(str);
-                boolstart = boolend = false;
-            }
-        }
-        return text.toString();
+    private boolean isEnd(int index) {
+        return indexIsZero(index) || indexIsZero(index + 1);
+    }
+
+    private boolean indexIsAboveThreshold(int index) {
+        return indexDistribution.get(index) > threshold;
+    }
+
+    private boolean indexIsZero(int index) {
+        return indexDistribution.get(index) != 0;
     }
 
     @Override
